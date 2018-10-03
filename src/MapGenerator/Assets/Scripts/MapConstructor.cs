7ef8217bf371d9b2c794e52da81b95ca6baca174
Dale Tubat <daletubat@yahoo.com>
2018-10-03 00:46:24 -0700

    Prevalance changes. Added "UsePieceMaterials" flag, only works before builds.

--- src/MapGenerator/Assets/Scripts/MapConstructor.cs
+++ src/MapGenerator/Assets/Scripts/MapConstructor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MapConstructor : MonoBehaviour
 {
 	public int Depth;
+	public bool UseMaterialColors;
 
 	public Dictionary<PieceCoordinates, MapNode> Map;
 	public MapNode OriginNode;
@@ -19,5 +20,13 @@ public class MapConstructor : MonoBehaviour
 		Map.Add(OriginNode.Coordinates, OriginNode);
 
 		OriginNode.Build(this, gameObject, Depth);
+		if(!UseMaterialColors)
+		{
+			Material newMat = new Material(Shader.Find("Standard"));
+			foreach(MeshRenderer mr in GetComponentsInChildren<MeshRenderer>())
+			{
+				mr.material = newMat;
+			}
+		}
 	}
 }
